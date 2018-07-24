3b1d9d55cae50edf1113f079245cacb140f9ec72
Setugekka <fzjie1006@sina.com>
2018-07-24 13:54:39 +0800

    18724fzj

--- src/app/dashboard/weather/weather.component.ts
+++ src/app/dashboard/weather/weather.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
@@ -10,6 +10,13 @@ export class WeatherComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $("#owl-demo,#owl-demo1,#owl-demo3,#owl-demo4").owlCarousel({
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      items : 4,
+      itemsDesktop : [768,3],
+      itemsDesktopSmall : [414,2]
+
+    });
   }
 
 }

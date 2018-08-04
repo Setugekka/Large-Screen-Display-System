835b9ea56a4fb9f9dc3df78e3ee500c832ff8c36
Setugekka <fzjie1006@sina.com>
2018-08-04 20:47:57 +0800

    1884fzj

--- src/app/dashboard/overview/overview.component.ts
+++ src/app/dashboard/overview/overview.component.ts
@@ -7,7 +7,43 @@ declare var $: any;
   styleUrls: ['./overview.component.css']
 })
 export class OverviewComponent implements OnInit {
+  option = {
 
+    // calculable : true,
+    series : [
+      {
+        name:'面积模式',
+        type:'pie',
+        radius : [15, 50],
+        // center : ['50%', '30%'],
+        label: {
+          normal: {
+            show: false,
+            position:"inside"
+          },
+          emphasis: {
+            show: true,
+          }
+        },
+        roseType : 'area',
+        data:[
+          {value:10, name:'季度1',itemStyle:{
+              color:"#73faf3"
+            }},
+          {value:5, name:'季度2',itemStyle:{
+              color:"#aed0fa"
+            }},
+          {value:15, name:'季度3',itemStyle:{
+              color:"#a6fab7"
+            }},
+          {value:25, name:'季度4',itemStyle:{
+              color:"#d2c7fa"
+            }},
+        ]
+
+      }
+    ]
+  };
   constructor() { }
 
   ngOnInit() {

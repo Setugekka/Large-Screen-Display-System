bd9544df6073c4e66849a3071ba21185367a6b84
Setugekka <fzjie1006@sina.com>
2018-09-07 15:22:05 +0800

    97update

--- src/app/shared/sidebar/sidebar-routes.config.ts
+++ src/app/shared/sidebar/sidebar-routes.config.ts
@@ -51,63 +51,63 @@ export const ROUTES: RouteInfo[] = [
       { path: '/TrainingExercises/CityTraining', title: '市县级', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
     ]
   },
-  {
-    path: '',
-    title: '应急科技支撑体系',
-    icon: 'ft-server',
-    class: 'has-sub',
-    badge: '',
-    badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1',
-    isExternalLink: false,
-    submenu: [
-      { path: '/TechnicalSupport/D5000', title: 'D5000系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/TechnicalSupport/Weather', title: '气象系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/TechnicalSupport/Irrigation', title: '水利系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/TechnicalSupport/Earthquake', title: '地震系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/TechnicalSupport/Firecontrol', title: '消防系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/TechnicalSupport/Police', title: '公安系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/TechnicalSupport/Medical', title: '医疗系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/TechnicalSupport/Traffic', title: '路况系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-    ]
-  },
-  {
-    path: '',
-    title: '处置救援能力',
-    icon: 'ft-settings',
-    class: 'has-sub',
-    badge: '',
-    badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1',
-    isExternalLink: false,
-    submenu: [
-      { path: '/DisposeRescue/Repair', title: '应急抢修队伍', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/DisposeRescue/Manager', title: '应急基干分队', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/DisposeRescue/Expert', title: '应急专家队伍', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/DisposeRescue/News', title: '新闻舆情队伍', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/DisposeRescue/Worker', title: '外来施工队伍', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-    ]
-  },
-  {
-    path: '',
-    title: '综合保障能力',
-    icon: 'ft-edit',
-    class: 'has-sub',
-    badge: '',
-    badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1',
-    isExternalLink: false,
-    submenu: [
-      { path: '/IntegratedSupportability/Material', title: '应急物资', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/IntegratedSupportability/Vehicle', title: '应急车辆', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/IntegratedSupportability/Equipment', title: '应急装备', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/IntegratedSupportability/LogisticsMaterial', title: '后勤保障物资', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/IntegratedSupportability/GeneratorCar', title: '应急发电车', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/IntegratedSupportability/Alternator', title: '应急照明', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/IntegratedSupportability/Lighting', title: '应急发电机', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-    ]
-  },
+  // {
+  //   path: '',
+  //   title: '应急科技支撑体系',
+  //   icon: 'ft-server',
+  //   class: 'has-sub',
+  //   badge: '',
+  //   badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1',
+  //   isExternalLink: false,
+  //   submenu: [
+  //     { path: '/TechnicalSupport/D5000', title: 'D5000系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/TechnicalSupport/Weather', title: '气象系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/TechnicalSupport/Irrigation', title: '水利系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/TechnicalSupport/Earthquake', title: '地震系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/TechnicalSupport/Firecontrol', title: '消防系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/TechnicalSupport/Police', title: '公安系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/TechnicalSupport/Medical', title: '医疗系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/TechnicalSupport/Traffic', title: '路况系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //   ]
+  // },
+  // {
+  //   path: '',
+  //   title: '处置救援能力',
+  //   icon: 'ft-settings',
+  //   class: 'has-sub',
+  //   badge: '',
+  //   badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1',
+  //   isExternalLink: false,
+  //   submenu: [
+  //     { path: '/DisposeRescue/Repair', title: '应急抢修队伍', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/DisposeRescue/Manager', title: '应急基干分队', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/DisposeRescue/Expert', title: '应急专家队伍', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/DisposeRescue/News', title: '新闻舆情队伍', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/DisposeRescue/Worker', title: '外来施工队伍', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //   ]
+  // },
+  // {
+  //   path: '',
+  //   title: '综合保障能力',
+  //   icon: 'ft-edit',
+  //   class: 'has-sub',
+  //   badge: '',
+  //   badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1',
+  //   isExternalLink: false,
+  //   submenu: [
+  //     { path: '/IntegratedSupportability/Material', title: '应急物资', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/IntegratedSupportability/Vehicle', title: '应急车辆', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/IntegratedSupportability/Equipment', title: '应急装备', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/IntegratedSupportability/LogisticsMaterial', title: '后勤保障物资', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/IntegratedSupportability/GeneratorCar', title: '应急发电车', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/IntegratedSupportability/Alternator', title: '应急照明', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //     { path: '/IntegratedSupportability/Lighting', title: '应急发电机', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+  //   ]
+  // },
   {
     path: '',
     title: '舆情应对能力',
-    icon: 'ft-airplay',
+    icon: 'ft-message-square',
     class: 'has-sub',
     badge: '',
     badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1',
@@ -120,18 +120,29 @@ export const ROUTES: RouteInfo[] = [
   {
     path: '',
     title: '预防监测和监控预警系统',
-    icon: 'ft-airplay',
+    icon: 'ft-cast',
     class: 'has-sub',
     badge: '',
     badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1',
     isExternalLink: false,
     submenu: [
       { path: '/PreventionMonitoring/VideoSurveillance', title: '视频监控系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/PreventionMonitoring/VideoTransmission', title: '视频传输系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+      // { path: '/PreventionMonitoring/VideoTransmission', title: '视频传输系统', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
       { path: '/PreventionMonitoring/SystemMap', title: '全省系统图及预警发布情况', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
       // { path: '/PreventionMonitoring/WarningSignals', title: '预警发布情况', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
       { path: '/DataManagement/InputFile', title: '数据模板', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
 
+    ]
+  },
+  {
+    path: '/ScreenDisplay/LargeScreen',
+    title: '大屏展示',
+    icon: 'ft-airplay',
+    class: '',
+    badge: '',
+    badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1',
+    isExternalLink: false,
+    submenu: [
     ]
   },
 ];
